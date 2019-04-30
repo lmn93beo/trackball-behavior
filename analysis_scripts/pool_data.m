b6c6d1ae8ee9e693fcb86a4a6abc30d8d4ed3c27
Nhat Le <lmn93beo@yahoo.com>
2019-04-30 14:19:07 -0400

    Included error bars and more flexible loading of files

--- analysis_scripts/pool_data.m
+++ analysis_scripts/pool_data.m
@@ -1,10 +1,44 @@
+% This script reads the raw .mat files in the specified folder
+% and pools all trials from the different sessions.
+% It plots the empirical performance and saves the pooled data
+% as a .mat file
+
 %% Find all behavior files with varying contrasts
 clear all
-parent_folder = 'C:\Users\Sur lab\Dropbox (MIT)\trackball-behavior\Data\80_91\91_RACC';
+mouse = input('Enter mouse number: ');
+
+switch getenv('computername')
+    case 'DESKTOP-FN1P6HD'
+        root = 'C:\Users\Sur lab\Dropbox (MIT)\trackball-behavior';
+    otherwise
+        root = 'C:\Users\Le\Dropbox (MIT)\trackball-behavior';
+end
+
+
+switch mouse
+    case 91
+        parent_folder = sprintf('%s%s', root, '\Data\80_91\91_RACC');
+    case 80
+        parent_folder = sprintf('%s%s', root, '\Data\80_91\80_LACC');
+    case '13L'
+        parent_folder = sprintf('%s%s', root, '\Data\C13\laser_analys_FebMar2019_left');
+    case '13R'
+        parent_folder = sprintf('%s%s', root, '\Data\C13\laser_analys_FebMar2019_right');
+    case '13Apr'
+        parent_folder = sprintf('%s%s', root, '\Data\C13\Apr2019');
+    otherwise
+        error('Invalid mouse number');
+end
+
+
 saveFileName = 'Mouse080_behavior.mat';
 load_all = 1; %Load all files, regardless of laser
 load_laser = 1; %load only files with laser
 checkstim = 0;
+
+colors = linspecer(2);
+colors(3,:) = [0 0 0];
+addpath(pwd);
 
 % Load info
 %% Find folders with correct trackball_data
@@ -22,12 +56,14 @@ for i = 1:size(twostim_files,2)
     else
         output = 1;
     end
-    fprintf('%d\n', output);
 
     if output
+        fprintf('Adding file %s...\n', twostim_files{i});
         animals(end+1) = data.mouse;
         k = k + 1;
         sessions{k} = twostim_files{i};
+    else
+        fprintf('REJECTED: %s...\n', twostim_files{i});
     end
 end
 
@@ -38,8 +74,7 @@ throw_away = [];
 for i = 1:numel(throw_away)
     all_animals(all_animals == throw_away(i)) = [];
 end
-colors = linspecer(2);
-colors(3,:) = [0 0 0];
+
 
 k = 0; all_cons = []; all_perf = [];
 figure(2);
@@ -69,12 +104,23 @@ for i = 1:numel(all_animals)
         con_perf_NL = get_twostim_perf(data, 0);
         con_perf_laser = get_twostim_perf(data, 1);
         
-        target_con = 0.64;
+        target_con = data.params.contrast;
+        disp(target_con);
+        
+        if target_con ~= 0.64
+            fprintf('Target contrast is not 0.64, skipping...\n');
+            continue;
+        end
+        
         [luminanceNL, perfNL, ntrialsNL, nleftNL] = get_perf_from_arr(con_perf_NL, target_con);
         [luminanceL, perfL, ntrialsL, nleftL] = get_perf_from_arr(con_perf_laser, target_con);
         
         Curr_perf_NL{ii} = perfNL;
         Curr_perf_laser{ii} = perfL;
+        NtrialsNL{ii} = ntrialsNL';
+        NleftNL{ii} = nleftNL';
+        NtrialsL{ii} = ntrialsL';
+        NleftL{ii} = nleftL';
          
         
         subplot(nrows, ncols, ii);
@@ -82,7 +128,7 @@ for i = 1:numel(all_animals)
         hold on;
         l2 = plot(luminanceNL, perfNL, 'b');
         
-        xlabel('Left - right stimulus luminance');
+        xlabel('Luminance diff.');
         ylabel('% left selected');
         
         filename = curr_files{ii};
@@ -107,4 +153,30 @@ for i = 1:numel(all_animals)
 end
 
 %% Perform an average across days
+ntrialsNL_arr = cell2mat(NtrialsNL);
+ntrialsL_arr = cell2mat(NtrialsL);
+nleftNL_arr = cell2mat(NleftNL);
+nleftL_arr = cell2mat(NleftL);
+
+ntrialsNL_total = sum(ntrialsNL_arr, 2);
+ntrialsL_total = sum(ntrialsL_arr, 2);
+nleftNL_total = sum(nleftNL_arr, 2);
+nleftL_total = sum(nleftL_arr, 2);
+
+perfNL_total = nleftNL_total ./ ntrialsNL_total;
+perfL_total = nleftL_total ./ ntrialsL_total;
+
+% Plot the aggregate performance
+figure;
+l1 = plot(luminanceL, perfL_total, 'o', 'Color', colors(1,:));
+hold on
+l2 = plot(luminanceNL, perfNL_total, 'o', 'Color', colors(2,:));
+
+%% Save the parameters
+filename = input('Enter the name of the file, 0 to skip saving: ');
+if ischar(filename)
+    save(filename, 'luminanceL', 'luminanceNL', ...
+        'nleftNL_total', 'nleftL_total',...
+        'ntrialsNL_total', 'ntrialsL_total');
+end
 
