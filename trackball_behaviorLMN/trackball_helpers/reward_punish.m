195e1c740017dbb698024fbf23a44938798b8ced
Nhat Le <lmn93beo@yahoo.com>
2020-01-21 11:50:37 -0500

    Cleaned up reward_punish.m

--- trackball_behaviorLMN/trackball_helpers/reward_punish.m
+++ trackball_behaviorLMN/trackball_helpers/reward_punish.m
@@ -13,7 +13,6 @@ end
 % Delay between correct choice and reward -- RH
 if sum(data.params.rwdDeliveryDelay) > 0
     if data.stimuli.loc(k) == choice %if correct
-        %psychsr_sound(6); %play reward sound
         rwdDelay = tic;
         currDelay = data.params.rwdDeliveryDelay(randi(numel(data.params.rwdDeliveryDelay)));
         while toc(rwdDelay) < currDelay
@@ -21,59 +20,28 @@ if sum(data.params.rwdDeliveryDelay) > 0
     end
 end
 
-
-id = 5;
-if choice == 5 | choice == 7 % timeout
+% Timeout
+if choice == 5 || choice == 7
     if data.params.MissSound
         if data.params.lever == 0
             psychsr_sound(1);
         end
     end
     data.response.reward(k) = 0;
-elseif choice == 6 % abort sound
-    psychsr_sound(18);
-    data.response.reward(k) = 0;
-elseif choice == data.stimuli.loc(k) || data.stimuli.loc(k)==3 % correct choice
-    data.params.reward = max(data.params.reward / 1, 3);
-    fprintf('reward decremented to %d\n', ...
-                    data.params.reward);
-               
     
-    
+% Correct choice
+elseif choice == data.stimuli.loc(k) || data.stimuli.loc(k)==3 
+
     if length(data.response.reward_time)==1
         id = 1;
     else
         id = choice;
     end
-        
-        
-%     elseif data.params.actionValue % left/right
-%         if data.stimuli.block(k) == 3 % equal value
-%             [~,id] = max(data.params.reward);
-%         else
-%             id = xor(choice-1,data.stimuli.block(k)-1)+1;
-%         end
-%     elseif data.params.linkStimAction % diamond/square with fixed values
-%         if data.stimuli.loc(k) == 3
-%             id = ~xor(choice-1,data.stimuli.id(k)-1)+1;
-%         else
-%             id = data.stimuli.id(k);
-%         end
-%     else % diamond/square with values changing as function of block
-%         if data.stimuli.loc(k) == 3            
-%             id = xor(~xor(choice-1,data.stimuli.id(k)-1),data.stimuli.block(k)-1)+1;
-%         else
-%             id = xor(data.stimuli.id(k)-1,data.stimuli.block(k)-1)+1;
-%         end
-%     end
 
     if rand < data.params.rewardProb(id) && ~(data.params.lever == 1 && data.stimuli.loc(k) == 1)
         % play reward sound
-        if length(data.params.reward) > 1 && data.params.reward(id) == max(data.params.reward) && data.params.rewardDblBeep
-            psychsr_sound(16);
-        elseif data.params.reward(id)>0 && ~(data.params.lever==1 && data.params.lev_chirp) 
-            psychsr_sound(6);
-        end
+        psychsr_sound(6);
+        
 
         % deliver reward            
         fprintf('%s REWARD %duL\n',datestr(toc(tstart)/86400,'MM:SS'),data.params.reward(id))
@@ -87,62 +55,14 @@ elseif choice == data.stimuli.loc(k) || data.stimuli.loc(k)==3 % correct choice
         data.response.reward(k) = data.params.reward(id);
 
         % potentially change block
-        nrewards = nrewards + 1;
-        if nrewards == rewardSwitch && length(data.params.reward)>1
-            nblocks = nblocks + 1;
-            nrewards = 0;
-
-            if data.params.firstBlockEqual
-                if nblocks == 1
-                    if sum(data.response.choice(1:k)==1) > sum(data.response.choice(1:k)==2)
-                        data.stimuli.block(k+1:end)=1; %left bias, reward right more
-                    else
-                        data.stimuli.block(k+1:end)=2;
-                    end                        
-                else
-                    data.stimuli.block(k+1:end) = 3-data.stimuli.block(k);
-                end
-            else                
-                data.stimuli.block(k+1:end) = bs(mod(nblocks,length(bs))+1);
-            end
-
-            % determine next block size
-            if length(data.params.blockSize)>1
-                rewardSwitch = randi(data.params.blockSize);
-            end
-
-            if data.params.actionValue && data.params.freeForcedBlocks 
-                if data.stimuli.block(k+1) < 3
-                    data.stimuli.loc(k+1:end) = 3;
-                else                    
-                    maxrepeat = floor(log(0.125)/log(abs(data.params.perRight-0.5)+0.5)); if maxrepeat<0; maxrepeat = Inf; end;
-                    data.stimuli.loc(k+1:end) = psychsr_rand(1-data.params.perRight,data.params.numTrials-k,0,maxrepeat);                        
-                    rewardSwitch = max(data.params.blockSize);
-                end                    
-            end
-
-        end
+        
     else
         fprintf('%s REWARD 0uL\n',datestr(toc(tstart)/86400,'MM:SS'))
         data.response.reward(k) = 0;
     end
-else % incorrect choice
-    new_reward = data.params.reward * 1;
-    if new_reward < 12
-        data.params.reward = new_reward;
-    end
-    fprintf('reward incremented to %d\n', ...
-                        data.params.reward)
-                    
-    if data.params.lever>0
-        if data.params.lev_pufftime > 0             
-            outdata = data.card.dio.UserData;
-            outdata.dt(2) = data.params.lev_pufftime;
-            outdata.tstart(2) = NaN;
-            data.card.dio.UserData = outdata;
-        end
-       
-    elseif data.params.incorrSound > 0
+% Incorrect choice  
+else                     
+    if data.params.incorrSound > 0
         psychsr_sound(1);
     end
     data.response.reward(k) = 0;
@@ -150,3 +70,5 @@ else % incorrect choice
     % Timeout
     pause(3)
 end
+
+% Switch to a new block if block switching condition is satisfied
